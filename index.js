06508e6cc3999e4543f053fac8effec4d682a710
Harris Kim <seura710@gmail.com>
2021-10-16 01:39:49 -0400

    commit tree 097340cca15e7e92b5c6e8320af362bd4129d4af

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((element) => {
+        let lowerString = string.toLowerCase();
+        let lowerElement = element.toLowerCase();
+        return lowerString === lowerElement;
+    })
+}
+
+
+   
+function fuzzyMatch(drivers, string) {
+    return drivers
+      .map((driver) => driver.split())
+      .filter((splitDriver) => splitDriver[0].indexOf(string) === string);
+  }
